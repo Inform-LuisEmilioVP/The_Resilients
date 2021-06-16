586f28055a9a08d66eac7ac3a1acd7593f5d61b8
Informar-LuisEmilioVP <informluisemiliovp@gmail.com>
2021-06-15 23:12:35 -0400

    Creación de la Sección de detalles de los Servicios.

--- Js/Main.js
+++ Js/Main.js
@@ -14,3 +14,40 @@ tabsContainer.addEventListener('click', (e) => {
 		aboutSection.querySelector(target).classList.add('active');
 	}
 });
+
+/*------------------ Services Item Details Popup ------------------*/
+document.addEventListener('click', (e) => {
+	if (e.target.classList.contains('see-more-btn')) {
+		toggleServicePopup();
+		document.querySelector('.services-popup').scrollTo(0, 0);
+		servicesItemDetails(e.target.parentElement);
+	}
+});
+
+function toggleServicePopup() {
+	document.querySelector('.services-popup').classList.toggle('open');
+	document.body.classList.toggle('hide-scrolling');
+	document.querySelector('.main').classList.toggle('fade-out');
+}
+
+document.querySelector('.pp-close').addEventListener('click', toggleServicePopup);
+
+//Hide popup when clicking outside of it
+document.addEventListener('click', (e) => {
+	if (e.target.classList.contains('pp-inner')) {
+		toggleServicePopup();
+	}
+});
+
+function servicesItemDetails(serviceItem) {
+	document.querySelector('.pp-thumbnail img').src = serviceItem.querySelector(
+		'.services-item-thumbnail img'
+	).src;
+
+	document.querySelector('.pp-header h3').innerHTML =
+		serviceItem.querySelector('.service-item-title').innerHTML;
+
+	document.querySelector('.pp-body').innerHTML = serviceItem.querySelector(
+		'.services-item-details'
+	).innerHTML;
+}
