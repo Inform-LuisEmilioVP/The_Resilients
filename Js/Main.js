c87060e023660388223b258b4bc4bb4814951caa
Informar-LuisEmilioVP <informluisemiliovp@gmail.com>
2021-06-22 23:08:56 -0400

    Interacción del Menú con las Secciones de la Pagina

--- Js/Main.js
+++ Js/Main.js
@@ -1,3 +1,39 @@
+/*------------------ Toggle Navbar ------------------*/
+const navToggler = document.querySelector('.nav-togger');
+navToggler.addEventListener('click', () => {
+	hideSection();
+	toggleNavbar();
+	document.body.classList.toggle('hide-scrolling');
+});
+
+function hideSection() {
+	document.querySelector('section.active').classList.toggle('fade-out');
+}
+function toggleNavbar() {
+	document.querySelector('.header').classList.toggle('active');
+}
+
+/*------------------ Active Section ------------------*/
+document.addEventListener('click', (e) => {
+	if (e.target.classList.contains('link-item') && e.target.hash !== '') {
+		navToggler.classList.add('hide');
+		if (e.target.classList.contains('nav-item')) {
+			toggleNavbar();
+		} else {
+			hideSection();
+			document.body.classList.add('hide-scrolling');
+		}
+		setTimeout(() => {
+			document
+				.querySelector('section.active')
+				.classList.remove('active', 'fade-out');
+			document.querySelector(e.target.hash).classList.add('active');
+			window.scrollTo(0, 0);
+			document.body.classList.remove('hide-scrolling');
+			navToggler.classList.remove('hide');
+		}, 500);
+	}
+});
 /*------------------ About Beginning ------------------*/
 const tabsContainer = document.querySelector('.about-beginning');
 const aboutSection = document.querySelector('.about-section');
